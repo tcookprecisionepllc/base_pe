b785483c2408febe8a1a0f384d818640d387d28e
Jose M. Coronado <j.coronado.personal@gmail.com>
2018-05-02 13:46:07 -0700

    [Initial Commit]

---
+++ models/stock_move.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    date_done = fields.Datetime('Closed Date',related='picking_id.date_done', store=True, readonly=True)
+    qty_available = fields.Float('QoH', related="product_id.qty_available",store=False,readonly=True)
