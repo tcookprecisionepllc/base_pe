502881c986d9d0dd80905c07872c28be725bb4e0
Tony Cook <tcook@precisionepllc.com>
2018-05-03 22:49:02 +0000

    Corrected xpath

--- models/stock_move.py
+++ models/stock_move.py
@@ -8,8 +8,5 @@ class StockMove(models.Model):
 
     date_done = fields.Datetime('Closed Date',related='picking_id.date_done', store=True, readonly=True)
     qty_available = fields.Float('QoH', related="product_id.qty_available",store=False,readonly=True)
-<<<<<<< HEAD
-    #product_desc = fields.Text(string="Description", related="product_id.description_picking")
-=======
+
     product_desc = fields.Text(string="Description",related="product_id.description_picking")
->>>>>>> 9c3ffd37ca885ee380d3c00cb69a515c3da19323
