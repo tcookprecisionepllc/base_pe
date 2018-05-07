7238717c4ae70c6fd8fd9400d7c2f4d7ba6e9eaf
Tony Cook <tcook@precisionepllc.com>
2018-05-07 10:39:17 -0700

    Correcting intialization of bin_locations to One2many

--- models/stock_move.py
+++ models/stock_move.py
@@ -10,4 +10,4 @@ class StockMove(models.Model):
     qty_available = fields.Float('QoH', related="product_id.qty_available",store=False,readonly=True)
 
     product_desc = fields.Text(string="Description",related="product_id.description_picking")
-    bin_locations = fields.Many2many(string="Bin Locations",related="product_id.x_shelf_location_tags")
+    bin_locations = fields.One2many(string="Bin Locations",related="product_id.x_shelf_location_tags")
