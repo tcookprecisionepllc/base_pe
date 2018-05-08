c6b85599e844fc3b30f8529fcdaec2291c83ca17
Tony Cook <tcook@precisionepllc.com>
2018-05-08 08:40:06 -0700

    Trial and error with declaration and join

--- models/stock_move.py
+++ models/stock_move.py
@@ -10,4 +10,5 @@ class StockMove(models.Model):
     qty_available = fields.Float('QoH', related="product_id.qty_available",store=False,readonly=True)
 
     product_desc = fields.Text(string="Description",related="product_id.description_picking")
-    bin_locations = fields.Many2many('x_shelf_locations',string="Bin Locations")
+    bin_locations = fields.Many2many(string="Bin Locations",related="product_id.x_shelf_location_tags")
+    #bin_locations = fields.Many2many('x_shelf_locations',string="Bin Locations")
