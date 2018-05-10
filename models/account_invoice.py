49af8d947898b0bd8f482d08b95466198159d351
Tony Cook <tcook@precisionepllc.com>
2018-05-10 11:55:21 -0700

    Adding emails to invoice

---
+++ models/account_invoice.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    invoice_address_email = fields.Char("Email Address", related="partner_id.email_temp")
+    shipping_address_email = fields.Char("Email Address", related="partner_shipping_id.email_temp")
