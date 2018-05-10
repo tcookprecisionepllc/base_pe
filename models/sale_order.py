7c9b888856b125c73c7b59ef974f5b0b796f355a
Tony Cook <tcook@precisionepllc.com>
2018-05-10 11:04:47 -0700

    Commenting out declaration

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,7 +3,7 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
+#class SaleOrder(models.Model):
+    #_inherit = "sale.order"
 
     #invoice_address_email = fields.Many2one("Email Address", related="partner_invoice_id.email_temp")
