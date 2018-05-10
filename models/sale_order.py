76ac7c618da854798656be4cf1120c7b951c80be
Tony Cook <tcook@precisionepllc.com>
2018-05-10 10:28:17 -0700

    Commenting out changes

--- models/sale_order.py
+++ models/sale_order.py
@@ -6,4 +6,4 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    invoice_address_email = fields.Many2one("Email Address", related="partner_invoice_id.email_temp")
+    #invoice_address_email = fields.Many2one("Email Address", related="partner_invoice_id.email_temp")
