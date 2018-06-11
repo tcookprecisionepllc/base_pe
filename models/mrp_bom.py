3dae959f52eae71654f864f983934aca6b604b9f
Tony Cook <tcook@precisionepllc.com>
2018-06-11 15:22:54 -0700

    Adding labor hours to BOM

--- models/mrp_bom.py
+++ models/mrp_bom.py
@@ -4,6 +4,6 @@ from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 class MrpBom(models.Model):
-    _inherit = "mrp.production"
+    _inherit = "mrp.bom"
 
     labor_hours = fields.Float("Labor Hours")
