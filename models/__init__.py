326e5ea6eebd9b97083664ca016a347544f1a92f
Tony Cook <tcook@precisionepllc.com>
2018-06-11 15:50:50 -0700

    Adding labor hours to BOM

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from . import stock_move,mrp_production,res_partner,sale_order,account_invoice,stock_picking,sale_order_line
+from . import stock_move
+from . import mrp_production
+from . import res_partner
+from . import sale_order
+from . import account_invoice
+from . import stock_picking
+from . import sale_order_line
+from . import mrp_bom
