9cdc580392ab04569ef12e81049f564ba0f9e8de
Tony Cook <tcook@precisionepllc.com>
2018-06-11 12:26:58 -0700

    Adding product internal category to sales order entry form

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import stock_move,mrp_production,res_partner,sale_order,account_invoice,stock_picking,product_category
+from . import stock_move,mrp_production,res_partner,sale_order,account_invoice,stock_picking,product_category,product_template
