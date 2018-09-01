b0aa870f6e346b21d4683e6411dc491d4c146270
Tony Cook <tcook@precisionepllc.com>
2018-08-31 21:05:59 -0700

    Adding descriptions to BOM Structure report lines

---
+++ models/mrp_bom_line.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+class MrpBomLine(models.Model):
+    _inherit = "mrp.bom.line"
+
+    description = fields.Text(string="Description", related="product_id.description_picking")
