d340dd9b4279592a44d2684c8f710f741492c5ca
Tony Cook <tcook@precisionepllc.com>
2018-09-11 08:37:37 -0700

    Adding invoice delivery fields to form and tree views

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -9,5 +9,5 @@ class AccountInvoice(models.Model):
     invoice_address_email = fields.Char("Email Address", related="partner_id.email_temp")
     shipping_address_email = fields.Char("Email Address", related="partner_shipping_id.email_temp")
     legacy_number = fields.Char("Legacy Number")
-    billing_method = fields.Selection([('email', _('Email')),('mail', _('Mail')),('fax', _('Fax')),('hand', _('Hand-delivered'))], string='Billing Method', default='email')
+    billing_method = fields.Selection([('email', _('Email')),('mail', _('Mail')),('fax', _('Fax')),('hand', _('Hand-delivered'))], string='Billing Method')
     date_delivered = fields.Date('Date Delivered')
