533114b2f294187c76847d0a7f18634e4f889518
Tony Cook <tcook@precisionepllc.com>
2018-09-28 14:32:28 -0700

    Adding custom cost fields to product form view

--- models/product_template.py
+++ models/product_template.py
@@ -7,5 +7,5 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     cost_custom = fields.Float('Custom Cost')
-    cost_entered_by = fields.Many2One('res.partner','Entered By')
+    cost_entered_by = fields.Many2one('res.partner','Entered By')
     cost_entered_date = fields.Date('Date Entered')
