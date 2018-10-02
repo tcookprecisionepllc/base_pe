b0ea9eb8b90c0560e9dccce6d44b0289c3e07cdc
Tony Cook <tcook@precisionepllc.com>
2018-10-02 10:17:11 -0700

    Adding Quantity Counted field to stock move form view

--- models/stock_move.py
+++ models/stock_move.py
@@ -14,6 +14,7 @@ class StockMove(models.Model):
     counted_by = fields.Many2many('res.partner', string="Counted By")
     reversing_move = fields.Many2one('stock.move', string="Reversing Move")
     is_reversing_move = fields.Boolean(string="Is Reversing Move")
+    qty_counted = fields.Float('Quantity Counted')
 
     #@api.model
     #def my_example(self):
