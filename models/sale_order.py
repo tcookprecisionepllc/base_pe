f6ed2323029d01797b74a99ec57e8b9b689a943a
Tony Cook <tcook@precisionepllc.com>
2018-10-25 07:58:17 -0700

    Replacing salesperson field on contact form view

--- models/sale_order.py
+++ models/sale_order.py
@@ -9,3 +9,5 @@ class SaleOrder(models.Model):
     invoice_address_email = fields.Char("Email Address", related="partner_invoice_id.email_temp")
     shipping_address_email = fields.Char("Email Address", related="partner_shipping_id.email_temp")
     sale_deadline = fields.Date('Sale Deadline')
+    user_id = fields.Many2one('res.users', string='Created By', index=True, track_visibility='onchange', default=lambda self: self.env.user)
+    salesperson_from_partner = fields.Char(string="Salesperson", related="partner_id.salesperson")
