03d1a93d087bee241aba26cccbb319187041c319
Tony Cook <tcook@precisionepllc.com>
2019-04-02 17:39:01 -0500

    Finalizing 301 redirect

--- controllers/main.py
+++ controllers/main.py
@@ -1,17 +1,8 @@
 from odoo import http
+from odoo.addons.website.controllers.main import Website
 
-from odoo.addons.portal.controllers.web import Home
-
-class Website(Home):
-
-    @http.route('/page/home', type='http', auth='public', website=True)
-    def index(self, **kw):
-        redirect = "http://www.precisionepllc.com/precisionepllc/"
-        return http.redirect_with_hash(redirect,code=301)
-
-class Redirects(http.Controller):
-
-    @http.route('/page/contactus', type='http', auth='public', website=True)
-    def contactus(self, **kw):
-        redirect = "http://www.precisionepllc.com/precisionepllc/contact-us/"
+class Redirects(Website):
+    @http.route('/page/<page:page>', auth='public', website=True)
+    def page(self, page, **opt):
+        redirect = "http://www.precisionepllc.com/precisionepllc"
         return http.redirect_with_hash(redirect,code=301)
