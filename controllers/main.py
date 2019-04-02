5ab92d1b48c87c380d613bac47d291a70b5b6185
Tony Cook <tcook@precisionepllc.com>
2019-04-02 16:16:43 -0500

    Testing 301 redirect

--- controllers/main.py
+++ controllers/main.py
@@ -4,14 +4,14 @@ from odoo.addons.portal.controllers.web import Home
 
 class Website(Home):
 
-    @http.route('/', type='http', auth='public', website=True)
+    @http.route('/page/home', type='http', auth='public', website=True)
     def index(self, **kw):
         redirect = "http://www.precisionepllc.com/precisionepllc/"
         return http.redirect_with_hash(redirect,code=301)
 
 class Redirects(http.Controller):
 
-    @http.route('/contactus', type='http', auth='public', website=True)
+    @http.route('/page/contactus', type='http', auth='public', website=True)
     def contactus(self, **kw):
         redirect = "http://www.precisionepllc.com/precisionepllc/contact-us/"
         return http.redirect_with_hash(redirect,code=301)
