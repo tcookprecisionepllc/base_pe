73006cff6163c68ca9385c136a01a6332abb7787
Tony Cook <tcook@precisionepllc.com>
2019-04-22 09:16:03 -0500

    Adding fields to PO and picking

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -7,3 +7,6 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     origin_custom = fields.Char("Custom Origin")
+    date_sent = fields.Datetime(string="Date Sent")
+    sent_by = fields.Many2one('res.partner','Sent By')
+    date_confirmed = field.Datetime(string="Date Confirmed")
--- models/stock_picking.py
+++ models/stock_picking.py
@@ -10,3 +10,6 @@ class StockMove(models.Model):
     corrective_move_no = fields.Char("Corrective Move")
     job_ticket_number = fields.Char(string='Job Ticket Number', related="sale_id.job_ticket_number")
     date_received = fields.Datetime(string="Date Received")
+    date_checked = fields.Datetime(string="Date Checked")
+    checked_by = fields.Many2one('res.partner','Checked By')
+    stocked_by = fields.Many2one('res.partner','Stocked By')
