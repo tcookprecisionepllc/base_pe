bdda917b93508bd2deea87b8ccf91b98c9ad063f
Tony Cook <tcook@precisionepllc.com>
2019-04-23 11:26:27 -0500

    Adding freight fields to picking view

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -15,3 +15,6 @@ class StockMove(models.Model):
     stocked_by = fields.Many2one('res.partner','Pulled/Stocked By')
     freight_cost = fields.Float(string="Freight Charge")
     freight_po = fields.Char(string="Freight PO")
+    weight_custom = fields.Float(string="Weight (lbs)")
+    carton_count_custom = fields.Integer(string="Number of Cartons")
+    carton_description = fields.Char(string="Carton Description")
