ceda04dc6f7c7958fa94c2c5f17e392e80cb0935
Tony Cook <tcook@precisionepllc.com>
2019-04-25 16:10:09 -0500

    Creating quotation report with internal notes

--- models/sale_order.py
+++ models/sale_order.py
@@ -12,3 +12,7 @@ class SaleOrder(models.Model):
     user_id = fields.Many2one('res.users', string='Created By', index=True, track_visibility='onchange', default=lambda self: self.env.user)
     salesperson_from_partner = fields.Char(string="Salesperson", related="partner_id.salesperson.display_name")
     job_ticket_number = fields.Char(string='Job Ticket Number')
+    date_rfq = fields.Datetime(string="RFQ Received")
+    date_quoted = fields.Datetime(string="Quote Sent")
+    quote_sent_by = fields.Many2one('res.partner', 'Quote Sent By')
+    date_ordered = fields.Datetime(string="Date Ordered")
