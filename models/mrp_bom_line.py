b4406ed0b1671b676b01466fd44c02198eca4af0
Tony Cook <tcook@precisionepllc.com>
2019-05-02 10:20:30 -0500

    Testing adding field to mrp.bom.lines

--- models/mrp_bom_line.py
+++ models/mrp_bom_line.py
@@ -7,4 +7,5 @@ class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
     description = fields.Text(string="Description", related="product_id.description_picking")
+    test = fields.Char(string="test")
     #1809011048 This field is not showing up in model. See ir_ui_views.xml for the BOM view to be changed
