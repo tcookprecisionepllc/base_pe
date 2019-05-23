efbd85d756cfda58337cab614858c67b594cdb6f
Tony Cook <tcook@precisionepllc.com>
2019-05-23 13:16:06 -0500

    Adding rig and asset number to Picking Operations report

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -23,3 +23,5 @@ class StockMove(models.Model):
     shipped_by = fields.Many2one('res.partner', "Shipped By")
     product_description = fields.Text(string="Description", related="product_id.description_picking")
     delivered_to = fields.Many2one('res.partner', "Delivered To")
+    asset_number = fields.Char(string='Asset Number', related="sale_id.asset_number")
+    rig_number = field.Char(string='Rig Number', related="sale_id.rig_number")
