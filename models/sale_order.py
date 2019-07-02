7626502385b9eb535ee3d9fdfe6307473c263e86
Tony Cook <tcook@precisionepllc.com>
2019-07-02 16:42:06 -0500

    Testing removal of problem models

--- models/sale_order.py
+++ models/sale_order.py
@@ -6,8 +6,8 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    invoice_address_email = fields.Char("Email Address", related="partner_invoice_id.email_temp")
-    shipping_address_email = fields.Char("Email Address", related="partner_shipping_id.email_temp")
+    #invoice_address_email = fields.Char("Email Address", related="partner_invoice_id.email_temp")
+    #shipping_address_email = fields.Char("Email Address", related="partner_shipping_id.email_temp")
     sale_deadline = fields.Date('Sale Deadline')
     user_id = fields.Many2one('res.users', string='Created By', index=True, track_visibility='onchange', default=lambda self: self.env.user)
     salesperson_from_partner = fields.Char(string="Salesperson", related="partner_id.salesperson.display_name")
