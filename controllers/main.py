2314859396c28522e668db5257935f20e6381f91
Tony Cook <tcook@precisionepllc.com>
2019-07-02 16:22:23 -0500

    Testing removal of problem models

--- controllers/main.py
+++ controllers/main.py
@@ -1,8 +1,8 @@
-from odoo import http
-from odoo.addons.website.controllers.main import Website
+#from odoo import http
+#from odoo.addons.website.controllers.main import Website
 
-class Redirects(Website):
-    @http.route('/page/<page:page>', auth='public', website=True)
-    def page(self, page, **opt):
-        redirect = "http://www.precisionepllc.com/precisionepllc"
-        return http.redirect_with_hash(redirect,code=301)
+#class Redirects(Website):
+#    @http.route('/page/<page:page>', auth='public', website=True)
+#    def page(self, page, **opt):
+#        redirect = "http://www.precisionepllc.com/precisionepllc"
+#        return http.redirect_with_hash(redirect,code=301)
