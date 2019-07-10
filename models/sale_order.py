f13aa6452502527499a2f0f1b9ec8a7784410265
Tony Cook <tcook@precisionepllc.com>
2019-07-10 17:57:07 -0500

    Testing incremtal reintroduction of fields

--- models/sale_order.py
+++ models/sale_order.py
@@ -10,7 +10,7 @@ class SaleOrder(models.Model):
     #shipping_address_email = fields.Char("Email Address", related="partner_shipping_id.email_temp")
     sale_deadline = fields.Date('Sale Deadline')
     user_id = fields.Many2one('res.users', string='Created By', index=True, track_visibility='onchange', default=lambda self: self.env.user)
-    #salesperson_from_partner = fields.Char(string="Salesperson", related="partner_id.salesperson.display_name")
+    salesperson_from_partner = fields.Char(string="Salesperson", related="partner_id.salesperson.display_name")
     job_ticket_number = fields.Char(string='Job Ticket Number')
     date_rfq = fields.Datetime(string="RFQ Received")
     date_quoted = fields.Datetime(string="Quote Sent")
