f4bbf696bf4d387cc20d0d7bbacdf09c06aca157
Tony Cook <tcook@precisionepllc.com>
2019-07-10 18:00:34 -0500

    Testing incremtal reintroduction of fields

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -6,8 +6,8 @@ from odoo.exceptions import UserError
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    #invoice_address_email = fields.Char("Email Address", related="partner_id.email_temp")
-    #shipping_address_email = fields.Char("Email Address", related="partner_shipping_id.email_temp")
+    invoice_address_email = fields.Char("Email Address", related="partner_id.email_temp")
+    shipping_address_email = fields.Char("Email Address", related="partner_shipping_id.email_temp")
     legacy_number = fields.Char("Legacy Number")
     billing_method = fields.Selection([('email', _('Email')),('mail', _('Mail')),('fax', _('Fax')),('hand', _('Hand-delivered'))], string='Billing Method')
     date_delivered = fields.Date('Date Delivered')
--- models/sale_order.py
+++ models/sale_order.py
@@ -6,7 +6,7 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    #invoice_address_email = fields.Char("Email Address", related="partner_invoice_id.email_temp")
+    invoice_address_email = fields.Char("Email Address", related="partner_invoice_id.email_temp")
     shipping_address_email = fields.Char("Email Address", related="partner_shipping_id.email_temp")
     sale_deadline = fields.Date('Sale Deadline')
     user_id = fields.Many2one('res.users', string='Created By', index=True, track_visibility='onchange', default=lambda self: self.env.user)
