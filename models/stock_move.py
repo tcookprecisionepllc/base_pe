d56bdc62b76999aff53eef4eadc25aaf3c50c8b6
root <root@odoo-test.c.bitnami-mhrmgedrmq.internal>
2019-07-29 23:24:38 +0000

    [IMP] Call changes

--- models/stock_move.py
+++ models/stock_move.py
@@ -8,6 +8,7 @@ class StockMove(models.Model):
 
     date_done = fields.Datetime('Closed Date',related='picking_id.date_done', store=True, readonly=True)
     qty_available = fields.Float('QoH', related="product_id.qty_available",store=False,readonly=True)
+    serial_number = fields.Char(string="Serial Number")
 
     product_desc = fields.Text(string="Description",related="product_id.description_picking")
     bin_locations = fields.Many2many(string="Bin Locations",related="product_id.x_shelf_location_tags")
