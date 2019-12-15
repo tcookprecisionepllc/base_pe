6226c4def41b10f5cacfc6cc2d15ab97eb3a81f6
Tony Cook <tcook@precisionepllc.com>
2019-12-15 09:30:18 -0600

    Integrating old changes into v12

--- models/stock_move_operation_link.py
+++ models/stock_move_operation_link.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+#from odoo import api, fields, models, _
+#from odoo.exceptions import UserError
 
-class StockMoveOperationLink(models.Model):
-    _inherit = "stock.move.operation.link"
+#class StockMoveOperationLink(models.Model):
+#    _inherit = "stock.move.operation.link"
 
-    price_unit = fields.Float("Unit Price", related="move_id.price_unit")
+#    price_unit = fields.Float("Unit Price", related="move_id.price_unit")
