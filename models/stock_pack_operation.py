1bb5fdd730762255fcff7dd06a0dad5a64b4f6ce
Tony Cook <tcook@precisionepllc.com>
2019-12-15 09:55:17 -0600

    Integrating old changes into v12

--- models/stock_pack_operation.py
+++ models/stock_pack_operation.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+#from odoo import api, fields, models, _
+#from odoo.exceptions import UserError
 
-class StockPackOperation(models.Model):
-    _inherit = "stock.pack.operation"
+#class StockPackOperation(models.Model):
+#    _inherit = "stock.pack.operation"
 
-    price_unit = fields.Float("Unit Price")
-    bin_locations = fields.Many2many(string="Bin Locations",related="product_id.x_shelf_location_tags")
-    product_description = fields.Text(string="Description", related="product_id.description_picking")
+#    price_unit = fields.Float("Unit Price")
+#    bin_locations = fields.Many2many(string="Bin Locations",related="product_id.x_shelf_location_tags")
+#    product_description = fields.Text(string="Description", related="product_id.description_picking")
